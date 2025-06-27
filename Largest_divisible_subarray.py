2a97c089f1ce20ac111df7dda81ec11f65fef68b
Vedant Badukale <149178970+Vedant988@users.noreply.github.com>
2025-06-27 23:44:20 +0530

    DSA

---
+++ Largest_divisible_subarray.py
@@ -0,0 +1,23 @@
+class Solution:
+    def largestSubset(self, arr):
+        arr = sorted(arr)
+        dp = [1]*len(arr)
+        prev = [-1] *len(arr)
+        
+        mIdx = 0
+        mLen = 0
+        
+        for i in range(len(arr)):
+            for j in range(0,i):
+                if arr[i]%arr[j]==0 and dp[j]+1>=dp[i]:
+                    dp[i] = dp[j] + 1
+                    prev[i] = j
+            if dp[i] > mLen:
+                mLen = dp[i]
+                mIdx = i
+        result = []
+        while mIdx != -1:
+            result.append(arr[mIdx])
+            mIdx = prev[mIdx]
+        return result[::-1]
+                    
