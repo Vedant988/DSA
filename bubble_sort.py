541b01793185c3a549a128f3c06d7aa9a7c144fe
Vedant Badukale <149178970+Vedant988@users.noreply.github.com>
2025-06-28 21:46:29 +0530

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,13 +1,17 @@
-arr = [13,46,24,52,20,9]
+arr = [9,10,12,13,15,16]
 n = len(arr)
 
 def BubbleSort(arr):
     for i in range(n-1,0,-1):
+        didswap=0
         for j in range(0,i):
             if arr[j]>arr[j+1]:
+                didswap+=1
                 temp = arr[j+1]
                 arr[j+1]=arr[j]
                 arr[j]=temp
+        if didswap==0:
+            return arr
             
     return arr
 arr = BubbleSort(arr)
