47f92dfe8613afce1adfafb3a3698afa62c7ffa1
Vedant Badukale <149178970+Vedant988@users.noreply.github.com>
2025-07-01 16:18:46 +0530

    freq_check.py

---
+++ Equal_freq.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sameFreq(self, s: str) -> bool:
+        freq = {}
+        for ch in s:
+            freq[ch] = freq.get(ch, 0) + 1
+        
+        lst = list(freq.values())
+        
+        if len(set(lst)) == 1:
+            return True
+        for i in range(len(lst)):
+            temp = lst.copy()
+            temp[i] -= 1
+            if temp[i] == 0:
+                temp.pop(i)
+            if len(set(temp)) == 1:
+                return True
+        return False
+        
