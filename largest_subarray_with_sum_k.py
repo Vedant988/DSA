2ed69abc778c32de85a2fd0885f641c0b5b1298d
Vedant Badukale <149178970+Vedant988@users.noreply.github.com>
2025-07-28 23:52:11 +0530

    largest_subarray_with_sum_k.py

---
+++ largest_subarray_with_sum_k.py
@@ -0,0 +1,15 @@
+def subarray_sumk(arr,k):
+    left = 0
+    combiner = 0
+    max_len = 0
+    for right in range(len(arr)):
+        combiner += arr[right]
+        while combiner>k and left<=right:
+            combiner-=arr[left]
+            left+=1
+        if combiner == k:
+            max_len = max(max_len, right - left + 1)
+    return max_len
+
+
+
